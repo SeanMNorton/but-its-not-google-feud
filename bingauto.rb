73f65630b9a47a73325f8c3f03619a28c7350daa
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-07-30 19:22:24 -0500

    cleaned up code, annotated

--- bingauto.rb
+++ bingauto.rb
@@ -4,7 +4,7 @@ require 'json'
 Dotenv.load
 
 
-def get_info
+def get_info  ## for command line program only
   puts "What are you searching?"
   request = gets.chomp
   body_parser(send_request(request))
@@ -13,30 +13,25 @@ end
 def send_request(request_string)
   uri = URI('https://api.cognitive.microsoft.com/bing/v5.0/suggestions/')
   uri.query = URI.encode_www_form({
-      # Request parameters
-      'q' => request_string
+      'q' => request_string # Request parameters
   })
 
   request = Net::HTTP::Get.new(uri.request_uri)
-  # Request headers
-  request['Ocp-Apim-Subscription-Key'] = ENV['KEY1'] #REMOVE KEY WHEN COMMITING
-  # Request body
-  request.body = "{body}"
-
+  request['Ocp-Apim-Subscription-Key'] = ENV['KEY1'] # Request headers
   response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
       return http.request(request)
   end
 end
 
 def body_parser(response)
-  num = 0
   suggestions = []
   JSON.parse(response.body)["suggestionGroups"][0]['searchSuggestions'].each do |item|
-    puts "#{num+=1}) #{item['displayText']}"
-    suggestions << "#{num}) #{item['displayText']}"
+    # enable for command line program
+    # puts "#{item['displayText']}"
+    suggestions << item['displayText']
   end
   suggestions
 end
 
-
-get_info
+# enable to make command line program work
+# get_info
